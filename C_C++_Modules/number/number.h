1eb97404161f7aee4ce565960c0eba298c8f3c34
ShifatHasanGNS <shifathasangns@gmail.com>
2020-08-12 18:55:19 +0600

    Modified

--- C_C++_Modules/number/number.h
+++ C_C++_Modules/number/number.h
@@ -106,7 +106,8 @@
 #define AND &
 #define NOT ~
 #define XOR ^
-#define For(i, start, end, step) for(int i=(start), i<(end); i += (step))
+#define For(i, start, end, step) for(int i=(start); i<(end); i += (step))
+#define For_(type, i, start, end, step) for(type i=(start); i<(end); i += (step))
 
 /*
     T Y P E D E F S
